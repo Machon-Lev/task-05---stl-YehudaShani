b1552a30c1a067ab67fe6cd4168cbd48100d721f
yehuda shani <yehudashani88@gmail.com>
2023-07-26 21:35:45 +0300

    all working, still has loops

--- include/DistanceCalculators.h
+++ include/DistanceCalculators.h
@@ -3,9 +3,11 @@
 class DistanceCalculator
 {
 public:
-	static double euclideanDistance(double x1, double x2, double y1, double y2) {
-		
-				return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	static double euclideanDistance(double x1, double x2, double y1, double y2) {		
+		return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	}
+	static double chebyshevDistance(double x1, double x2, double y1, double y2) {
+		return std::max(abs(x1 - x2), abs(y1 - y2));
 	}
 	static double manhattanDistance(double x1, double x2, double y1, double y2) {
 		return abs(x1 - x2) + abs(y1 - y2);
